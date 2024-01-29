3bd1dec66f39934077bb1707e4c1b9197c3a5265
SantiagoRodR <asantiagor54@gmail.com>
2024-01-28 20:39:11 -0500

    JS basic operations and string operations

---
+++ Curso preparatorio Java/operadores.js
@@ -0,0 +1,37 @@
+//Suma
+
+console.log(3 + 5);
+
+//Resta
+
+console.log(14 - 7);
+
+//Multiplicación
+
+console.log(4 * 6);
+
+//División
+
+console.log(30 / 9);
+
+//Modúlo o residuo
+
+console.log(95 % 4);
+
+//Operaciones compuestas
+
+console.log(3 + 5 * 2 - 8);
+
+//para cambiar la prioridad de precedencia se usan los parentesis
+
+console.log((3 + 5) * 2 - 8);
+
+//Adición con strings
+
+console.log("Hola " + "Camilo");
+
+var nombre = "Camilo";
+var saludo = "Hola " + nombre;
+
+console.log(saludo);
+
